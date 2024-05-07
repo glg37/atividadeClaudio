cbdb49779f5ee1f5fc55ec1a9eff386e3b41b312
VictorFerreira69 <victor13.09.08ferreira@gmail.com>
2024-05-07 15:24:25 -0300

    codigo do player ded Victor

--- atividadeClaudio/Assets/PLayer.cs
+++ atividadeClaudio/Assets/PLayer.cs
@@ -2,46 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PLayer : MonoBehaviour
+public class player : MonoBehaviour
 {
-    
-    
+   
     public BoxCollider2D boxCollider;
     public SpriteRenderer spriteRenderer;
     public GameObject bullet;
     public Transform foot;
-    bool groundCheck;
+    public bool groundCheck;
     public float speed = 5, jumpStrength = 5, bulletSpeed = 8;
     public float horizontal;
     public Rigidbody2D body;
-    public float velocity;
-    int direction = 1;
+    int direction;
+    
+    
     // Start is called before the first frame update
     void Start()
+    {
+
+       
+    }
+
+    // Update is called once per frame
+    void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
         body.velocity = new Vector2(horizontal * speed, body.velocity.y);
         groundCheck = Physics2D.OverlapCircle(foot.position, 0.05f);
-
         if (Input.GetButtonDown("Jump") && groundCheck)
         {
             body.AddForce(new Vector2(0, jumpStrength * 100));
-        }
-        if (horizontal != 0)
-        {
-            direction = (int)horizontal;
         }
         if (Input.GetButtonDown("Fire1"))
         {
             GameObject temp = Instantiate(bullet, transform.position, transform.rotation);
             temp.GetComponent<Rigidbody2D>().velocity = new Vector2(bulletSpeed * direction, 0);
-
         }
-        if (Input.GetButtonDown("Fire2"))
+        if (Input.GetButtonDown("Fire3"))
         {
             speed = speed * 2;
         }
-    }
-   
-}
 
+        if (horizontal != 0)
+        {
+            direction = (int)horizontal;
+        }
+    }
+}
